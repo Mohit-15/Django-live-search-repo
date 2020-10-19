bae78355fe2ae4ab28a51fe4ca799495ca8c809c
Mohit-15 <programs.mohit15@gmail.com>
2020-10-20 01:11:46 +0530

    add model Search

--- live/models.py
+++ live/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+	name = models.CharField(max_length = 40)
+	description = models.CharField(max_length = 70)
+	url = models.URLField(max_length=200)
+
+	def __str__(self):
+		return self.name
