8fe4eace1e683fc113596d89441d674ad3f845b7
Mohit-15 <programs.mohit15@gmail.com>
2020-10-21 14:21:04 +0530

    create class based view

--- live/views.py
+++ live/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Service
+from django.views.generic import ListView
+import json
 
 # Create your views here.
-def home(request):
-	return HttpResponse("<h1>Hello There</h1>")
+# def home(request):
+# 	data = Service.objects.all()
+# 	return render(request, "main.html", {'context': data})
+
+class InfoListView(ListView):
+    model = Service
+    template_name = 'main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #convert python dictionary to Json
+        context["qs_json"] = json.dumps(list(Service.objects.values()))   
+        return context
 
